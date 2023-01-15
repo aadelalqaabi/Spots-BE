acf3e7f18ffed6e63e7f4da4e3b09b1ac74ec0a9
aa0241 <77753344+aa0241@users.noreply.github.com>
2023-01-15 04:30:13 +0300

    Fixed code model

--- models/Code.js
+++ models/Code.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const CodeSchema = new mongoose.Schema({
   code: String,
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Organizer",
+  },
 });
 
 module.exports = mongoose.model("Code", CodeSchema);
