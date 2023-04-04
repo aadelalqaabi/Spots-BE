e3d5edab9f4a5a7fd2986c1b00b3705a5b98e1bf
aa0241 <77753344+aa0241@users.noreply.github.com>
2023-04-05 01:54:22 +0300

    Fixed token dup

--- api/pushNotification/pushNotification.controllers.js
+++ api/pushNotification/pushNotification.controllers.js
@@ -2,6 +2,7 @@ const PushNotification = require("../../models/PushNotification");
 const Spot = require("../../models/Spot");
 const User = require("../../models/User");
 const { email } = require("../../middleware/email");
+const fetch = require("node-fetch");
 
 exports.fetchpushNotification = async (pushNotificationId, next) => {
   try {
@@ -18,37 +19,44 @@ exports.pushNotificationCreate = async (req, res, next) => {
   try {
     const newPushNotification = await PushNotification.create(req.body);
     const users = await User.find();
-    const filteredUsers = users.filter((user) => user.notificationToken !== "");
+    const filteredUsers = users.filter(
+      (user) => user.notificationToken !== "" || user.notificationToken !== null
+    );
+    let notificationsTokens = [];
 
-    for (let i = 0; i < filteredUsers.length; i++) {
+    for (let i = 0; i <= filteredUsers.length; i++) {
       let user = filteredUsers[i];
-      if (user.locale.includes(newPushNotification.locale)) {
-        let message = {
-          to: user.notificationToken,
-          sound: "default",
-          title: newPushNotification.title,
-          body: newPushNotification.body,
-          data: {},
-          _displayInForeground: true,
-        };
-        try {
-          let response = await fetch("https://exp.host/--/api/v2/push/send", {
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "Accept-encoding": "gzip, deflate",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(message),
-          });
-
-          if (!response.ok) {
-            throw new Error(
-              `Failed to send push notification to ${user.notificationToken}. Status: ${response.status}`
-            );
+      if (
+        !notificationsTokens.some((token) => token === user.notificationToken)
+      ) {
+        notificationsTokens.push(user.notificationToken);
+        if (user.locale.includes(newPushNotification.locale)) {
+          let message = {
+            to: user.notificationToken,
+            sound: "default",
+            title: newPushNotification.title,
+            body: newPushNotification.body,
+            data: {},
+            _displayInForeground: true,
+          };
+          try {
+            let response = await fetch("https://exp.host/--/api/v2/push/send", {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Accept-encoding": "gzip, deflate",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(message),
+            });
+            if (response.statusText !== "OK") {
+              throw new Error(
+                `Failed to send push notification to ${user.notificationToken}. Status: ${response.status}`
+              );
+            }
+          } catch (error) {
+            console.error(error);
           }
-        } catch (error) {
-          console.error(error);
         }
       }
     }
